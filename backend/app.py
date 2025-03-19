af64d7065157fb3a523379ffbb2cbe184ebbdd9c
bishal4965 <aryalbishal49@gmail.com>
2025-03-19 12:41:36 +0545

    Removed unnecessary comments

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# For demonstration, we're using SQLite. Update the URI to point to your MySQL DB if needed.
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
